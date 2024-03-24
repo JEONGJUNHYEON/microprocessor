9761c7152adc11dc285bbea304d913f92dfe51d4
최보열 <gju06051@gmail.com>
2024-03-25 02:37:08 +0900

    Add files via upload

---
+++ mac-p2.s
@@ -0,0 +1,32 @@
+@ Copyright (c) 2024 Jungrae Kim <dale40@skku.edu>
+@ This file is for programming assignments of micro-processor class at SungKyunKwan University.
+
+@ This template mimics a C program of
+@ #include <stdio.h>
+@
+@ int A[] = {1, 2, 3, 4};
+@ int B[] = {2, 4, 6, 8};
+@
+@ int main()
+@ {
+@     printf("Output is %d\n", A[0]*B[0]);
+@     return 0;
+@ }
+
+.global _start
+_start:
+@ Your codes for single element multiplication
+				@ Use a LDR pseudo instruction to set r2 to vectorA address
+				@ Use a LDR pseudo instruction to set r3 to vectorB address
+
+				@ Use a LDR normal instruction to load the first element of vector A to r4
+				@ Use a LDR normal instruction to load the first element of vector B to r5
+
+				@ Use a MUL instruction to multiply r4 and r5 and save the result to r1
+
+@ Codes for initialize vector data in memory
+.data
+vectorA:
+	.word	1, 2, 3, 4	@ Vector A = {1, 2, 3, 4}
+vectorB:
+	.word	2, 4, 6, 8	@ Vector B = {2, 4, 6, 8}
---
+++ mac-p4.s
@@ -0,0 +1,58 @@
+@ Copyright (c) 2024 Jungrae Kim <dale40@skku.edu>
+@ This file is for programming assignments of micro-processor class at SungKyunKwan University.
+
+@ This template mimics a C program of
+@ #include <stdio.h>
+@
+@ int A[] = {1, 2, 3, 4};
+@ int B[] = {2, 4, 6, 8};
+@
+@ int main()
+@ {
+@     printf("Output is %d\n", A[0]*B[0]+A[1]*B[1]+A[2]*B[2]+A[3]*B[3]);
+@     return 0;
+@ }
+
+
+.global _start
+_start:
+@ Your codes for vector multiplication using auto-update address
+				@ Reset r1, which will serve as a storage of sum, to 0
+
+				@ Use a LDR pseudo instruction to set r2 to vectorA address
+				@ Use a LDR pseudo instruction to set r3 to vectorB address
+
+				@ Use a LDR normal instruction to load the first element of vector A to r4
+				@ (auto increment r2 by 4 for the next element)
+				@ Use a LDR normal instruction to load the first element of vector B to r5
+				@ (auto increment r3 by 4 for the next element)
+
+				@ Use a MLA instruction to multiply r4 and r5 and accumulate the result to r1
+				@ (r1 will be the second argument of the printf("Output is %d", n) call).
+
+				@ Use a LDR normal instruction to load the second element of vector A to r4
+				@ (auto increment r2 by 4 for the next element)
+				@ Use a LDR normal instruction to load the second element of vector B to r5
+				@ (auto increment r3 by 4 for the next element)
+
+				@ Use a MLA instruction to multiply r4 and r5 and accumulate the result to r1
+
+				@ Use a LDR normal instruction to load the third element of vector A to r4
+				@ (auto increment r2 by 4 for the next element)
+				@ Use a LDR normal instruction to load the third element of vector B to r5
+				@ (auto increment r3 by 4 for the next element)
+
+				@ Use a MLA instruction to multiply r4 and r5 and accumulate the result to r1
+
+				@ Use a LDR normal instruction to load the third element of vector A to r4
+				@ (auto increment r2 by 4 for the next element)
+				@ Use a LDR normal instruction to load the third element of vector B to r5
+				@ (auto increment r3 by 4 for the next element)
+
+				@ Use a MLA instruction to multiply r4 and r5 and accumulate the result to r1
+
+.data
+vectorA:
+	.word	1, 2, 3, 4	@ Vector A = {1, 2, 3, 4}
+vectorB:
+	.word	2, 4, 6, 8	@ Vector B = {2, 4, 6, 8}
